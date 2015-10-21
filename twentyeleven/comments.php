7c2bb0082fd9d8eefa06ead62092ce7a26e016b5
Rahjon Lang <rahjon.lang@F1006-26-34158.bmcc.cuny.edu>
2015-10-20 21:24:01 -0400

    added note

--- twentyeleven/comments.php
+++ twentyeleven/comments.php
@@ -12,6 +12,14 @@
  * @since Twenty Eleven 1.0
  */
 ?>
+
+
+	<?php
+
+
+		//COMMENT
+
+	?>
 	<div id="comments">
 	<?php if ( post_password_required() ) : ?>
 		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
